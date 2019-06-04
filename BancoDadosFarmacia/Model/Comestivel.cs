73b5caa411c865cfd35b029de37372dfb80f99ba
Guilherme Ribeiro <48655138+guilhermelRibeiroo@users.noreply.github.com>
2019-06-04 16:23:50 -0300

    Adicionado a classe Comestivel na class library Model.

---
+++ BancoDadosFarmacia/Model/Comestivel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Comestivel
+    {
+        public int ID, Quantidade;
+        public string Nome, Marca;
+        public decimal Valor;
+        public DateTime Vencimento; 
+    }
+}
